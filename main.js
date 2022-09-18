42085299310ec38a424cff96b09962ce43be8b5b
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2022-09-18 16:36:23 -0400

    passing all

--- main.js
+++ main.js
@@ -3,78 +3,113 @@ const select = document.querySelector('select');
 const info = document.querySelector('#info');
 const shoutout = document.querySelector('#shoutout');
 const header4 = document.querySelector('h4');
+const body = document.querySelector('body');
+const ul = document.querySelector('ul');
+const form = document.querySelector('form');
 
+let fetchCall;
+// function populateDropDown() {
 fetch(url)
   .then((res) => res.json())
   .then((data) => {
     // console.log(data);
+    fetchCall = data;
+    // console.log(fetchCall);
     for (let i = 0; i < data.length; i++) {
       const name = data[i].name;
+      const age = data[i].age;
       const createOpt = document.createElement('option');
       createOpt.textContent = name;
-      createOpt.value = data[i].id;
+      createOpt.value = name;
       select.append(createOpt);
     }
   })
   .catch((err) => console.log(err));
 
-select.addEventListener('change', (e) => {
-  fetch(`${url}/${select.value}`)
-    .then((res) => res.json())
-    .then((data) => {
-      info.innerHTML = '';
-      const h4 = document.createElement('h4');
-      // h4.innerHTML = select.options[select.selectedIndex].value;
-      h4.innerHTML = `Name: ${data.name}`;
+//! select add event listener
 
-      const paragraph1 = document.createElement('p');
-      paragraph1.innerHTML = `Age: ${data.age}`;
+select.addEventListener('change', () => {
+  const callBack = fetchCall.find(
+    (element) => element.name === `${select.value}`
+  );
+  console.log(callBack);
 
-      const paragraph2 = document.createElement('p');
-      paragraph2.innerHTML = `Eyes: ${data.eye_color}`;
+  info.innerHTML = '';
+  const h4 = document.createElement('h4');
 
-      const paragraph3 = document.createElement('p');
-      paragraph3.innerHTML = `Hair Color: ${data.hair_color}`;
+  h4.innerHTML = `Name: ${callBack.name}`;
 
-      info.append(h4, paragraph1, paragraph2, paragraph3);
+  const paragraph1 = document.createElement('p');
+  paragraph1.innerHTML = `Age: ${callBack.age}`;
 
-      const form = document.querySelector('form');
+  const paragraph2 = document.createElement('p');
+  paragraph2.innerHTML = `Eyes: ${callBack.eye_color}`;
 
-      form.addEventListener('submit', (e) => {
-        e.preventDefault();
+  const paragraph3 = document.createElement('p');
+  paragraph3.innerHTML = `Hair Color: ${callBack.hair_color}`;
 
-        const name = data.name;
+  info.append(h4, paragraph1, paragraph2, paragraph3);
+});
 
-        const shoutoutValue = shoutout.value;
+//! form add event listener
 
-        const ul = document.querySelector('ul');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const name = `${select.value}`;
+  const shoutoutValue = shoutout.value;
+  // console.log(shoutoutValue);
 
-        const list = document.createElement('li');
-        list.innerHTML = '';
+  const list = document.createElement('li');
+  list.innerHTML = `<strong>${name}:</strong> ${shoutoutValue}`;
+  ul.append(list);
 
-        list.innerHTML = `<strong>${name}:</strong> ${shoutoutValue}`;
-        ul.append(list);
+  const errorP1 = document.createElement('p');
+  errorP1.innerHTML = '';
+  // errorP1.classList.add('error');
+  errorP1.innerHTML = `Please Select a Person`;
+  const errorP = document.createElement('p');
+  errorP.innerHTML = '';
+  errorP.classList.add('error');
+  errorP.innerHTML = `Please add a shoutout for ${name}`;
+  if (!`${select.value}`) {
+    errorP1.style.display = 'block';
+    errorP1.style.color = 'red';
+    errorP1.style.fontWeight = 'bold';
+    errorP1.style.fontSize = '13px';
+    form.append(errorP1);
+    select.addEventListener('click', () => {
+      errorP1.innerHTML = '';
+    });
 
-        const errorP = document.createElement('p');
-        errorP.innerHTML = '';
-        errorP.classList.add('error');
-        errorP.innerHTML = `Please add a shoutout for ${name}`;
-        // if(!)
-        if (name && shoutoutValue) {
-          errorP.style.display = ' none';
-          return;
-        }
-        if (name && !shoutoutValue) {
-          errorP.style.display = 'block';
-          errorP.style.color = 'red';
-          // errorP.style.fontWeight = 'bold';
-          errorP.style.fontSize = '13px';
-          form.append(errorP);
-          return;
-        }
+    return;
+  }
 
-        form.reset();
-      });
-    })
-    .catch((err) => console.log(err));
+  if (!shoutoutValue) {
+    errorP.style.display = 'block';
+    errorP.style.color = 'red';
+    errorP.style.fontWeight = 'bold';
+    errorP.style.fontSize = '13px';
+    form.append(errorP);
+    shoutout.addEventListener('click', () => {
+      errorP.innerHTML = '';
+    });
+    return;
+  } else if (shoutoutValue) {
+    errorP.style.display = 'none';
+    return;
+  }
+
+  form.reset();
+});
+
+//! remove button
+const button = document.createElement('button');
+button.setAttribute('id', 'reset-shoutouts');
+button.innerText = 'Remove Shoutouts';
+
+// button.style.textAlign = 'center';
+body.append(button);
+button.addEventListener('click', () => {
+  // const ul = document.getElementsByTagName('ul');
+  ul.innerHTML = '';
 });
